a3a3117d719562ce768f5251cf04ca2c8993c6fd
embryobirthday <alexinverda@gmail.com>
2024-07-29 21:35:30 -0400

    short qs completed

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,18 +1,27 @@
 // Recursion Problem Set: Coding Exercises
 
 // Calculate and return the total sum using recursive approach
-const sum = (arr) => {}
+const sum = (arr) => {
+    //guard clause in case that the array is empty; or base case
+    if (arr.length === 0) {
+        return 0
+    }
+
+    // recursive case - sum the first element and the sum of the rest of the array
+    return arr[0] + sum(arr.slice(1));
+
+}
 
 // Reverse string using recursive approach
-const reverse = (str) => {};
+const reverse = (str) => { };
 
 // Iterative approach to solving the fibonacci problem
-const fibIter = (n) => {};
+const fibIter = (n) => { };
 
 // Recursive fibonacci
-const fibRec = (n) => {};
+const fibRec = (n) => { };
 
 // Return the index of target in arr, or -1 if not found using recursion 
-const binarySearch = (arr, target, start = 0, end = arr.length - 1) => {};
+const binarySearch = (arr, target, start = 0, end = arr.length - 1) => { };
 
 module.exports = { sum, reverse, fibRec, fibIter, binarySearch };
