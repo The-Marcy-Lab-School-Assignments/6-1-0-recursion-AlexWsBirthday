0bc2f8d275866394d12ae43a2fc0da54a4c2ce85
Carmen Salas <carmensalas0114@gmail.com>
2024-08-06 15:34:26 -0400

    gave feedback

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -2,26 +2,27 @@
 
 // Calculate and return the total sum using recursive approach
 const sum = (arr) => {
-    //guard clause in case that the array is empty; or base case
-    if (arr.length === 0) {
-        return 0
-    }
+  //guard clause in case that the array is empty; or base case
+  if (arr.length === 0) {
+    return 0;
+  }
 
-    // recursive case - sum the first element and the sum of the rest of the array
-    return arr[0] + sum(arr.slice(1));
+  // recursive case - sum the first element and the sum of the rest of the array
+  return arr[0] + sum(arr.slice(1));
+};
 
-}
-
+// Refer to the recorded lecture and lab review from 7/29 in the Nirvana technical repo. We reviewed reverse and both fibonacci functions in those recordings.
 // Reverse string using recursive approach
-const reverse = (str) => { };
+const reverse = (str) => {};
 
 // Iterative approach to solving the fibonacci problem
-const fibIter = (n) => { };
+const fibIter = (n) => {};
 
 // Recursive fibonacci
-const fibRec = (n) => { };
+const fibRec = (n) => {};
 
-// Return the index of target in arr, or -1 if not found using recursion 
-const binarySearch = (arr, target, start = 0, end = arr.length - 1) => { };
+// Watch this short video to help with binarySearch: https://www.youtube.com/watch?v=EFXWgZJZqL8&t=351s
+// Return the index of target in arr, or -1 if not found using recursion
+const binarySearch = (arr, target, start = 0, end = arr.length - 1) => {};
 
 module.exports = { sum, reverse, fibRec, fibIter, binarySearch };
